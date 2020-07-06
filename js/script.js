d3f1c488a8a50d23b75dadbbcb5d860e4bce6d80
JosefinE7 <fin.jos@hotmail.com>
2020-07-06 17:11:45 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".box").on("click", function () {
+        /* when we click on a element that has a "box" class, this event will be fired. */
+        var classNames = $(this).attr("class").split(" ");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+            /* if this object is already red, it turns black*/
+            $("." + className).css("background-color", "#000");
+        } else {
+            // else turn all elements with a box class black
+            // and then change the color of all elements
+            // with the same class name as the clicked element
+            // to red.
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+    });
+});
